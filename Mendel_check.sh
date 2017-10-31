f692c63e2cdc4a00d9c95490b59f9ef820a2d849
Emily Vonderharr <emilyvonderharr@agro200953237.cfans.umn.edu>
2017-10-31 11:32:59 -0500

    Mendel checker

---
+++ Mendel_check.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+#   Emily Vonderharr 10/31/17
+#   this script is for plink Mendel error checking
+#   run from directory containing .ped files
+
+PLINK=~/Apps/plink
+MAP=/panfs/roc/groups/9/morrellp/vonde026/Deleterious_GP/Data/Mendel_Errors/From_ExCap/GP_Comb.map
+
+for i in *.ped
+do
+$PLINK --ped $i --map $MAP --allow-extra-chr --mendel --out ${i}_out
+done
